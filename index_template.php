e991e046b0d65b949b2bc46c49d53f7ef2a5f757
Julien Martin <martin.julien82@gmail.com>
2016-04-20 18:22:04 +0200

    Remove text for icons

--- index_template.php
+++ index_template.php
@@ -50,12 +50,21 @@ if (isset($_GET['element'])) {
 <body>
   <div id="content">
     <div>
-      <svg fill="#6C868E" height="96" viewBox="0 0 24 24" width="96" xmlns="http://www.w3.org/2000/svg">
+      <svg fill="#6C868E" height="96" viewBox="0 0 24 24" width="96" style="display:inline-block;" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 0h24v24H0z" fill="none"/>
+          <path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/>
+      </svg>
+
+      <svg fill="#6C868E" height="64" viewBox="0 0 24 24" width="64" style="display:inline-block; margin-bottom:16px" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 0h24v24H0z" fill="none"/>
+          <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"/>
+      </svg>
+
+      <svg fill="#6C868E" height="96" viewBox="0 0 24 24" width="96" style="display:inline-block;" xmlns="http://www.w3.org/2000/svg">
         <path d="M0 0h24v24H0z" fill="none"/>
         <path d="M20 4H4v2h16V4zm1 10v-2l-1-5H4l-1 5v2h1v6h10v-6h4v6h2v-6h1zm-9 4H6v-4h6v4z"/>
       </svg>
     </div>
-    <p>Please wait while we are preparing the installation</p>
     <div class="spinner">
       <div class="bounce1"></div>
       <div class="bounce2"></div>
@@ -66,7 +75,7 @@ if (isset($_GET['element'])) {
   <script type="text/javascript" src="index.php?element=jquery"></script>
   <script type="text/javascript">
     $(function() {
-
+/*
       var request = $.ajax({
         method: "POST",
         url: "index.php",
@@ -89,7 +98,7 @@ if (isset($_GET['element'])) {
         $('#error').html('An error has occured');
         $('.spinner').remove();
       });
-
+*/
     });
   </script>
 </body>
